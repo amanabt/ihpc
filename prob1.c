7bee8b4068522e36bdee283a7fd61f5b230023cc
chimp <aman2official@gmail.com>
2016-03-09 23:40:44 +0530

    Input function added working properly

--- prob1.c
+++ prob1.c
@@ -19,17 +19,17 @@ int* alloc_1d (int N, int M)
 	return data;
 }
 
-void read (int* A, int N, int M)
-{
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			for (int k = 0; k < M; ++k)
-				scanf ("%d", &A [(j * M + k) + (i * M * M)]);
-			scanf ("\n");
-		};
-		scanf ("\n");
-	}
-}
+// void read (int* A, int N, int M)
+// {
+// 	for (int i = 0; i < N; ++i) {
+// 		for (int j = 0; j < M; ++j) {
+// 			for (int k = 0; k < M; ++k)
+// 				scanf ("%d", &A [(j * M + k) + (i * M * M)]);
+// 			scanf ("\n");
+// 		};
+// 		scanf ("\n");
+// 	}
+// }
 	
 int main (int argc, char** argv)
 {
@@ -39,8 +39,8 @@ int main (int argc, char** argv)
 	int size_;
 	int ec_;
 	const int MPI_MASTER = 0;
+	int* A;
 	
-	int* A = alloc_1d (N, M);
 	ec_ = MPI_Init (&argc, &argv);
 	
 	if (ec_ != MPI_SUCCESS)
@@ -49,9 +49,55 @@ int main (int argc, char** argv)
 	MPI_Comm_size (MPI_COMM_WORLD, &size_);
 	MPI_Comm_rank (MPI_COMM_WORLD, &rank_);
 	
-	scanf ("%d\n%d", &N, &M);
-	if (rank_ == MPI_MASTER)
-		printf ("%d %d\n", N, M);
+	if (rank_ == MPI_MASTER) {
+		scanf ("%d\n%d", &N, &M);
+	}
+	
+	if (rank_ == MPI_MASTER) {
+		for (int rank = 1; rank < size_; ++rank) {
+			MPI_Send (&N, 1, MPI_INT, rank, MATRICES, MPI_COMM_WORLD);
+			MPI_Send (&M, 1, MPI_INT, rank, SIZE, MPI_COMM_WORLD);
+// 			MPI_Send (A, M * M * N, MPI_INT, rank, DATA, MPI_COMM_WORLD);
+		}
+	}
+
+	else {
+	MPI_Recv (&N,
+				1,
+				MPI_INT,
+				MPI_MASTER,
+				MATRICES,
+				MPI_COMM_WORLD,
+				MPI_STATUS_IGNORE);
+	
+	MPI_Recv (&M,
+				1,
+				MPI_INT,
+				MPI_MASTER,
+				SIZE,
+				MPI_COMM_WORLD,
+				MPI_STATUS_IGNORE);
+	
+	printf ("%d : %d : %d\n", rank_, N, M);
+	}
+	
+	A = alloc_1d (N, M);
+	
+	if (rank_ == MPI_MASTER) {
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < M; ++j) {
+				for (int k = 0; k < M; ++k)
+					scanf ("%d", &A [(j * M + k) + (i * M * M)]);
+// 				printf ("\n");
+			};
+// 			printf ("\n");
+		}
+		for (int i = 0; i < N * M * M; ++i)
+			printf ("%d ", A[i]);
+	}
+	
+	
+	
 // // 	printf ("%d", rank_);
 // // 	if (rank_ == 0)
 // // 		printf ("%d", rank_);
@@ -67,30 +113,30 @@ int main (int argc, char** argv)
 // 		
 // 		
 // // 	
-// // 	if (rank_ == MPI_MASTER) {
-// // 		for (int rank = 1; rank < nop_; ++rank) {
-// // 			MPI_Send (&M, 1, MPI_INT, rank, MATRICES, MPI_COMM_WORLD);
-// // 			MPI_Send (&N, 1, MPI_INT, rank, SIZE, MPI_COMM_WORLD);
-// // 			MPI_Send (A, M * M * N, MPI_INT, rank, DATA, MPI_COMM_WORLD);
-// // 		}
-// // 	}
+// 	if (rank_ == MPI_MASTER) {
+// 		for (int rank = 1; rank < nop_; ++rank) {
+// 			MPI_Send (&M, 1, MPI_INT, rank, MATRICES, MPI_COMM_WORLD);
+// 			MPI_Send (&N, 1, MPI_INT, rank, SIZE, MPI_COMM_WORLD);
+// 			MPI_Send (A, M * M * N, MPI_INT, rank, DATA, MPI_COMM_WORLD);
+// 		}
+// 	}
 // // 		
-// // 	else {
-// // 		MPI_Recv (&N,
-// // 				  1,
-// // 				  MPI_INT,
-// // 				  MPI_MASTER,
-// // 				  MATRICES,
-// // 				  MPI_COMM_WORLD,
-// // 				  MPI_STATUS_IGNORE);
-// // 		
-// // 		MPI_Recv (&M,
-// // 				  1,
-// // 				  MPI_INT,
-// // 				  MPI_MASTER,
-// // 				  SIZE,
-// // 				  MPI_COMM_WORLD,
-// // 				  MPI_STATUS_IGNORE);
+// 	else {
+// 		MPI_Recv (&N,
+// 				  1,
+// 				  MPI_INT,
+// 				  MPI_MASTER,
+// 				  MATRICES,
+// 				  MPI_COMM_WORLD,
+// 				  MPI_STATUS_IGNORE);
+// 		
+// 		MPI_Recv (&M,
+// 				  1,
+// 				  MPI_INT,
+// 				  MPI_MASTER,
+// 				  SIZE,
+// 				  MPI_COMM_WORLD,
+// 				  MPI_STATUS_IGNORE);
 // // 		
 // // 		MPI_Recv (A,
 // // 				  M * M * N,
